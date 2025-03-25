93c34fe2b055c81e22e042bd40c3eecf1e23cc07
J0nas-B <jonas.busch@online.de>
2025-03-25 15:27:46 +0100

    Labyrinth Game Client

---
+++ LabyrinthClient/src/main/java/com/example/LabyrinthClient/LabyrinthClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.LabyrinthClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabyrinthClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabyrinthClientApplication.class, args);
+	}
+
+}
